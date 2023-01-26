73fd1712d627b5dd45aec162a55b4352364024c0
Zane <zeisen33@gmail.com>
2023-01-26 15:12:44 -0500

    Produce Reducer and Action Creator

---
+++ practice-for-week-15-react-redux-grocery-store-long-practice-main/src/store/produce.js
@@ -0,0 +1,24 @@
+import produceData from '../mockData/produce.json'
+
+
+const POPULATE = 'produce/POPULATE'
+
+export default function produceReducer(state={}, action) {
+    const nextState = {...state}
+
+    switch (action.type) {
+        case POPULATE:
+            return {...nextState, ...action.produce}
+        default:
+            return nextState;
+    }
+}
+
+export const populateProduce = () => {
+    debugger
+    return {
+        type: POPULATE,
+        produce: produceData
+    }
+}
+
